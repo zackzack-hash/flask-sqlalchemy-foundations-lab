9ccf6a14d1b7c7b287c1c180dd452fb70947ed55
zakaria <azaki6777@gmail.com>
2026-09-06 22:28:17 +0300

    final commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,8 +20,36 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if not earthquake:
+        return make_response(jsonify({'message': f'Earthquake {id} not found.'}), 404)
+    
+    return make_response(
+        jsonify({
+            'id': earthquake.id,
+            'magnitude': earthquake.magnitude,
+            'location': earthquake.location,
+            'year': earthquake.year
+        }), 200)
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
 
+    quake_list = [
+        {
+            'id': quake.id,
+            'magnitude': quake.magnitude,
+            'location': quake.location,
+            'year': quake.year
+        } for quake in earthquakes
+    ]
+
+    return make_response(jsonify({
+        "count": len(quake_list),
+        "quakes": quake_list
+    }), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -5,4 +5,12 @@ metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Earthquake(db.Model):
+    __tablename__ = 'earthquakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    def __repr__(self):
+        return f'<Earthquake {self.id}, Magnitude: {self.magnitude}, Location: {self.location}, Year: {self.year}>'
